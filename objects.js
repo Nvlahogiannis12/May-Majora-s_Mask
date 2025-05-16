1b13c0190f577a81c035ede7bb6ca03dcff49cbc
Nvlahogiannis12 <526Nvlahogiannis@frhsd.com>
2025-05-16 22:43:53 +0000

    Info Added

---
+++ objects.js
@@ -0,0 +1,205 @@
+const townsfolk = [
+{
+    Name: 'Bombers',
+    Role: 'Secret Society of Justice',
+},
+{
+    Name:'Anju',
+    Role:'Stock Pot Inn Innkeeper',
+},
+{
+    Name:'Kafei',
+    Role:'Mysterious Youth',
+},
+{
+    Name:'Curiosity Shop Keeper',
+    Role:'Shop Keeper',
+},
+{
+    Name:'Old Lady',
+    Role:'Bomb Shop Owner',
+},
+{
+    Name:'Romani',
+    Role:'Young Farm Girl',
+},
+{
+    Name:'Cremia',
+    Role:'Manager of Romani Ranch',
+},
+{
+    Name:'Mr. Dotour',
+    Role:'Mayor of Clock Town',
+},
+{
+    Name:'Madame Aroma',
+    Role:'Mayor Wife',
+},
+{
+    Name:'Toto',
+    Role:'Manager of Indigo-Gos',
+},
+{
+    Name:'Gorman',
+    Role:'Leader of Traveling troupe',
+},
+{
+    Name:'Postman',
+    Role:'Mailman',
+},
+{
+    Name:'Rosa Sisters',
+    Role:'Twin Dancers in Gorman Troupe',
+},
+{
+    Name:'???',
+    Role:'Mysterious hand found in Stockpot Inn',
+},
+{
+    Name:'Anju\'\s Grandmother',
+    Role:'Storyteller',
+},
+{
+    Name:'Kamaro',
+    Role:'Ghost of a Dancer',
+},
+{
+    Name:'Grog',
+    Role:'Cucco Breeder',
+},
+{
+    Name:'Gorman Brothers',
+    Role:'Suspicious Horse Trainers',
+},
+{
+    Name:'Shiro',
+    Role:'Soldier',
+},
+{
+    Name:'Guru-Guru',
+    Role:'Gorman Troupe Musician',
+},
+]
+
+const masks = [
+{
+    Name:'Deku Mask',
+    Obtained:'Obtained By: Breaking Majora\'\s Curse on Link',
+    Purpose:'Allows Link to Transform into a Deku Scrub',
+},
+{
+    Name:'Goron Mask',
+    Obtained:'Obtained By: Healing the Goron Hero Darmani III\'\s soul',
+    Purpose:'Allows Link to Transform into a Goron',
+},
+{
+    Name:'Zora Mask',
+    Obtained:'Obtained By: Helping Mikau the Zora rest his soul',
+    Purpose:'Allows Link to Transform into a Zora',
+},
+{
+    Name:'Fierce Deity Mask',
+    Obtained:'Obtained By: Solving all of Clock Town\'\s problems',
+    Purpose:'Allows Link to Transform into the Fierce Deity',
+},
+{
+    Name:'Postman\'\s Hat',
+    Obtained:'Obtained By: Convincing the Post Man to run away and escape the Moon',
+    Purpose:'Allows Link to deliver letters and check Mailboxes',
+},
+{
+    Name:'All-Night Mask',
+    Obtained:'Obtained By: Purchased from the Curiosity Shop on the Third Night',
+    Purpose:'Allows Link to stay awake during Anju\'\s Grandmother\'\s stories',
+},
+{
+    Name:'Blast Mask',
+    Obtained:'Obtained By: Saving the Old Lady from a thief',
+    Purpose:'Allows Link to cause Explosions',
+},
+{
+    Name:'Stone Mask',
+    Obtained:'Obtained By: Helping Shiro',
+    Purpose:'Allows Link to be invisible to guards in various locations',
+},
+{
+    Name:'Great Fairy Mask',
+    Obtained:'Obtained By: Returning a Fairy to the Great Fairy',
+    Purpose:'Allows Link to attack stray fairies',
+},
+{
+    Name:'Keaton Mask',
+    Obtained:'Obtained By: Finding and speaking to Kafei',
+    Purpose:'Allows Link to meet a Keaton',
+},
+{
+    Name:'Bremen Mask',
+    Obtained:'Obtained By: Hearing Guru-Guru\'\s story',
+    Purpose:'Allows Link to help fulfill Grog\'\s',
+},
+{
+    Name:'Bunny Hood',
+    Obtained:'Obtained By: Fulfilling Grog\'\s wish',
+    Purpose:'Allows Link to run faster and jump farther',
+},
+{
+    Name:'Don Gero\'\s Mask',
+    Obtained:'Obtained By: Feeding the stranded Goron on the Mountains',
+    Purpose:'Allows Link to assemble the Frog Choir',
+},
+{
+    Name:'Mask of Scents',
+    Obtained:'Obtained By: Racing the Deku Butler',
+    Purpose:'Allows Link to find magic mushrooms for the Witches',
+},
+{
+    Name:'Romani\'\s Mask',
+    Obtained:'Obtained By: Escorting Romani and her Milk order to Clock Town',
+    Purpose:'Allows Link to enter the Milk Bar',
+},
+{
+    Name:'Circus Leader\'\s Mask',
+    Obtained:'Obtained By: Play a song in the Milk Bar',
+    Purpose:'Allows Link to cheer up Gorman Brothers',
+},
+{
+    Name:'Kafei\'\s Mask',
+    Obtained:'Obtained By: Accept the Quest to look for Kafei',
+    Purpose:'Allows Link to ask the Townsfolk about Kafei',
+},
+{
+    Name:'Couple\'\s Mask',
+    Obtained:'Obtained By: Reuniting Kafei and Anju',
+    Purpose:'Allows Link to stop the fighting in Town Hall',
+},
+{
+    Name:'Mask of Truth',
+    Obtained:'Obtained By: Break the Swamp Spider House\'\ Curse',
+    Purpose:'Allows Link to speak to animals and Gossip Stones',
+},
+{
+    Name:'Kamaro\'\s Mask',
+    Obtained:'Obtained By: Finding Kamaro\'\s Spirit',
+    Purpose:'Allows Link to teach the Rosa Sisters to dance',
+},
+{
+    Name:'Gibdo Mask',
+    Obtained:'Obtained By: Saving Pamela\'\s Father in Ikana Valley',
+    Purpose:'Allows Link to Redeads and Gibdos',
+},
+{
+    Name:'Garo\'\s Mask',
+    Obtained:'Obtained By: Beat the Gorman Brothers in a race',
+    Purpose:'Allows Link to enter Ikana Canyon',
+},
+{
+    Name:'Captain\'\s Hat',
+    Obtained:'Obtained By: Awaken and Defeat Captain Keeta of Ikana',
+    Purpose:'Allows Link to speak to undead soldiers',
+},
+{
+    Name:'Giant\'\s Mask',
+    Obtained:'Obtained By: Fighting the Stone Temple Boss',
+    Purpose:'Allows Link to turn into a giant',
+},
+]
